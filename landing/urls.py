7cb1c62762a8d456070d7af6b52978dfae2e2abb
Anas Bellouzi <anbellouzi@gmail.com>
2021-01-09 14:15:33 -0500

    updated search

--- landing/urls.py
+++ landing/urls.py
@@ -4,14 +4,13 @@ from .views import *
   
 urlpatterns = [
   path('', HomePageView.as_view(), name='home'),
-  path('search/', SearchPageView.as_view(), name='search'),
-  path('property/', PropertyPageView.as_view(), name='property'),
+  path('search/', search, name='search'),
+  path('property/<str:property_id>', PropertyPageView.as_view(), name='property'),
   path('search-results/', SearchResultsPageView.as_view(), name='search-results'),
   path('soon/', SoonPageView.as_view(), name='soon'),
   path('pricing/', PricingPageView.as_view(), name='pricing'),
   path('projects/', ProjectsPageView.as_view(), name='projects'),
   path('project-single/', ProjectSinglePageView.as_view(), name='project-single'),
   path('maintenance/', MaintenancePageView.as_view(), name='maintenance'),
-  path('a/', FilterPage.as_view(), name='plsword'),
 ]
   
