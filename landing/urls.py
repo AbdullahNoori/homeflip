dbd060274c43810478db9cf3c698c7a1896a5ba9
Anas Bellouzi <anbellouzi@gmail.com>
2021-01-10 01:35:05 -0500

    updated search url

--- landing/urls.py
+++ landing/urls.py
@@ -4,12 +4,13 @@ from .views import *
   
 urlpatterns = [
   path('', HomePageView.as_view(), name='home'),
-  path('search/', search, name='search'),
+  path('search', search, name='search'),
   path('property/<str:property_id>', PropertyPageView.as_view(), name='property'),
   path('search-results/', SearchResultsPageView.as_view(), name='search-results'),
   path('soon/', SoonPageView.as_view(), name='soon'),
   path('pricing/', PricingPageView.as_view(), name='pricing'),
   path('projects/', ProjectsPageView.as_view(), name='projects'),
+  path('contact/', ContactPageView.as_view(), name='contact'),
   path('project-single/', ProjectSinglePageView.as_view(), name='project-single'),
   path('maintenance/', MaintenancePageView.as_view(), name='maintenance'),
 ]
